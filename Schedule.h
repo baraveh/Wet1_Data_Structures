62e5e79edf233d544bb42b7cc7fa605239c3e05d
baraveh <baraveh@gmail.com>
2019-04-26 23:53:01 +0300

    finished all I could without tree

--- Schedule.h
+++ Schedule.h
@@ -13,17 +13,20 @@
 typedef enum {
     SCHEDULE_SUCCESS = 0,
     SCHEDULE_FAILURE = -1,
+    SCHEDULE_MEMORY_ERROR = -2,
     SCHEDULE_INVALID_INPUT = -3
 } ScheduleResult;
+
+#define NO_COURSE -1
 
 class Schedule {
 private:
 
-    struct arrayEntry{
-        Node<int>* roomPtr_m;
+    struct arrayEntry {
+        Node<int> *roomPtr_m;
         int courseId_m;
     };
-    
+
     int numOfRooms_m;
     int numOfHours_m;
 
@@ -49,13 +52,13 @@ public:
                                              numOfLessons_m(0),
                                              roomsArr_m(rooms, 0),
                                              hoursArr_m(hours, 0) {
-        
+
         availabilityPerHour_m = Array<List<int>>(numOfHours_m);
         lectureArr_m = Array<Array<arrayEntry>>(numOfHours_m);
-        for(int i =0; i < numOfHours_m; i++){
-            for(int j = 0; j < numOfRooms_m; j++){
+        for (int i = 0; i < numOfHours_m; i++) {
+            for (int j = 0; j < numOfRooms_m; j++) {
                 arrayEntry temp = {availabilityPerHour_m[i].addLast(j),
-                                   -1};
+                                   NO_COURSE};
                 lectureArr_m[i][j] = temp;
             }
         }
@@ -68,16 +71,19 @@ public:
         numOfLessons_m = aSchedule.numOfLessons_m;
         freeRooms_m = aSchedule.freeRooms_m;
         freeHours_m = aSchedule.freeHours_m;
-        courseTree_m = AvlTree<int, AvlTree<Lecture, int>>(aSchedule.courseTree_m);
+        courseTree_m = AvlTree<int, AvlTree<Lecture, int>>(
+                aSchedule.courseTree_m);
 
         availabilityPerHour_m = Array<List<int>>(numOfHours_m);
-        lectureArr_m = Array<Array<arrayEntry>>(numOfHours_m, Array<arrayEntry>(numOfRooms_m));
+        lectureArr_m = Array<Array<arrayEntry>>(numOfHours_m, Array<arrayEntry>(
+                numOfRooms_m));
 
-        for(int i =0 ; i < numOfHours_m; i++){
-            Node<int>* listIterator = aSchedule.availabilityPerHour_m[i].getHead();
-            for(int j = 0; j < numOfRooms_m; j++){
-                arrayEntry temp = {availabilityPerHour_m[i].addLast(listIterator->data_m),
-                                   aSchedule.lectureArr_m[i][j].courseId_m};
+        for (int i = 0; i < numOfHours_m; i++) {
+            Node<int> *listIterator = aSchedule.availabilityPerHour_m[i].getHead();
+            for (int j = 0; j < numOfRooms_m; j++) {
+                arrayEntry temp = {
+                        availabilityPerHour_m[i].addLast(listIterator->data_m),
+                        aSchedule.lectureArr_m[i][j].courseId_m};
                 lectureArr_m[i][j] = temp;
                 listIterator = listIterator->next_m;
             }
@@ -95,21 +101,23 @@ public:
         freeRooms_m = aSchedule.freeRooms_m;
         freeHours_m = aSchedule.freeHours_m;
 
-        courseTree_m = AvlTree<int, AvlTree<Lecture, int>>(aSchedule.courseTree_m);
+        courseTree_m = AvlTree<int, AvlTree<Lecture, int>>(
+                aSchedule.courseTree_m);
 
         availabilityPerHour_m = Array<List<int>>(numOfHours_m);
-        lectureArr_m = Array<Array<arrayEntry>>(numOfHours_m, Array<arrayEntry>(numOfRooms_m));
+        lectureArr_m = Array<Array<arrayEntry>>(numOfHours_m, Array<arrayEntry>(
+                numOfRooms_m));
 
-        for(int i =0 ; i < numOfHours_m; i++){
-            Node<int>* listIterator = aSchedule.availabilityPerHour_m[i].getHead();
-            for(int j = 0; j < numOfRooms_m; j++){
-                arrayEntry temp = {availabilityPerHour_m[i].addLast(listIterator->data_m),
-                                   aSchedule.lectureArr_m[i][j].courseId_m};
+        for (int i = 0; i < numOfHours_m; i++) {
+            Node<int> *listIterator = aSchedule.availabilityPerHour_m[i].getHead();
+            for (int j = 0; j < numOfRooms_m; j++) {
+                arrayEntry temp = {
+                        availabilityPerHour_m[i].addLast(listIterator->data_m),
+                        aSchedule.lectureArr_m[i][j].courseId_m};
                 lectureArr_m[i][j] = temp;
                 listIterator = listIterator->next_m;
             }
         }
-
 
     }
 
@@ -119,10 +127,28 @@ public:
             hour >= numOfHours_m) {
             return SCHEDULE_INVALID_INPUT;
         }
-        if (array_m[room][hour] != nullptr) {
+        if (lectureArr_m[room][hour].courseId_m != NO_COURSE) {
             return SCHEDULE_FAILURE;
         }
 
+        roomsArr_m[room]++;
+        hoursArr_m[hour]++;
+        numOfLessons_m++;
+        if (roomsArr_m[room] == 1) {
+            //first lesson in this room
+            freeRooms_m--;
+        }
+        if (hoursArr_m[hour] == 1) {
+            //first lesson in this hour
+            freeHours_m--;
+        }
+
+        availabilityPerHour_m[hour].moveNodeToEnd(
+                lectureArr_m[hour][room].roomPtr_m);
+
+        lectureArr_m[hour][room].courseId_m = courseId;
+        Lecture newLecture = Lecture(hour, room, courseId);
+        //TODO - add lecture to tree
     }
 
     ScheduleResult
@@ -132,11 +158,11 @@ public:
             hour >= numOfHours_m) {
             return SCHEDULE_INVALID_INPUT;
         }
-        if (array_m[room][hour] == nullptr) {
+        if (lectureArr_m[room][hour].courseId_m == NO_COURSE) {
             return SCHEDULE_FAILURE;
         }
 
-        *courseId = (array_m[room][hour])->key;
+        *courseId = lectureArr_m[room][hour].courseId_m;
         return SCHEDULE_SUCCESS;
     }
 
@@ -145,14 +171,74 @@ public:
             hour >= numOfHours_m) {
             return SCHEDULE_INVALID_INPUT;
         }
-        if (array_m[roomId][hour] == nullptr) {
+        if (lectureArr_m[roomId][hour].courseId_m == NO_COURSE) {
             return SCHEDULE_FAILURE;
 
         }
+
+        roomsArr_m[roomId]--;
+        hoursArr_m[hour]--;
+        numOfLessons_m--;
+        if (roomsArr_m[roomId] == 0) {
+            //last lesson in this room
+            freeRooms_m++;
+        }
+        if (hoursArr_m[hour] == 0) {
+            //last lesson in this hour
+            freeHours_m++;
+        }
+
+        availabilityPerHour_m[hour].moveNodeToStart(
+                lectureArr_m[hour][roomId].roomPtr_m);
+        lectureArr_m[hour][roomId].courseId_m = NO_COURSE;
+
+        //TODO - remove lecture from tree
     }
 
     ScheduleResult changeCourseId(const int &oldCourse, const int &newCourse) {
 
+    }
+
+    ScheduleResult calculateScheduleEfficiency(float *efficiency) {
+        if (efficiency == nullptr) {
+            return SCHEDULE_INVALID_INPUT;
+        }
+        if (numOfLessons_m == 0) {
+            return SCHEDULE_FAILURE;
+        }
+        *efficiency = (numOfLessons_m / ((numOfRooms_m - freeRooms_m) *
+                                         (numOfHours_m - freeHours_m)));
+        return SCHEDULE_SUCCESS;
+    }
+
+    ScheduleResult
+    getAllFreeRoomsByHour(int hour, int **rooms, int *numOfRooms) {
+        if (hour < 0 || hour >= numOfHours_m || numOfRooms == nullptr ||
+            rooms == nullptr) {
+            return SCHEDULE_INVALID_INPUT;
+        }
+
+        *numOfRooms = numOfRooms_m - hoursArr_m[hour];
+        int* freeRooms = (int*)malloc(sizeof(*freeRooms)*(*numOfRooms));
+        if(!freeRooms){
+            return SCHEDULE_MEMORY_ERROR;
+        }
+
+        Node<int>* listIterator = availabilityPerHour_m[hour].getHead();
+        for(int i = 0; i < *numOfRooms; i++){
+            freeRooms[i] = listIterator->data_m;
+            listIterator = listIterator->next_m;
+        }
+
+        *rooms = freeRooms;
+    }
+
+    ScheduleResult getAllLecturesByCourse(int courseId, int** hours, int** rooms, int* numOfLectures){
+        if(courseId <= 0 || hours == nullptr || rooms == nullptr || numOfLectures ==
+                                                                            nullptr){
+            return SCHEDULE_INVALID_INPUT;
+        }
+        //TODO
     }
 
 };
--- library.cpp
+++ library.cpp
@@ -4,12 +4,65 @@
 
 #include "library.h"
 #include "Schedule.h"
-void *Init(int hours, int rooms) {
-    Schedule *DS = new Schedule(hours, rooms);
+void*Init(int hours, int rooms) {
+    auto * DS = new Schedule(hours, rooms);
     return (void*)DS;
 }
 StatusType AddLecture(void *DS, int hour, int roomID, int courseID) {
-    return ((Schedule *)DS)-> AddLecture(hour, roomID, courseID);
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule *)DS)-> addLecture(hour, roomID, courseID));
 }
+
+StatusType GetCourseID(void *DS, int hour, int roomID, int *courseID) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->getCourseId(hour, roomID, courseID));
+}
+
+StatusType DeleteLecture(void *DS, int hour, int roomID) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->deleteLecture(hour, roomID));
+}
+
+StatusType ChangeCourseID(void *DS, int oldCourseID, int newCourseID) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->changeCourseId(oldCourseID, newCourseID));
+}
+
+StatusType CalculateScheduleEfficiency(void *DS, float *efficiency) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->calculateScheduleEfficiency(efficiency));
+}
+
+StatusType
+GetAllFreeRoomsByHour(void *DS, int hour, int **rooms, int *numOfRooms) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->getAllFreeRoomsByHour(hour, rooms, numOfRooms));
+}
+
+StatusType
+GetAllLecturesByCourse(void *DS, int courseID, int **hours, int **rooms,
+                       int *numOfLectures) {
+    if(!DS){
+        return INVALID_INPUT;
+    }
+    return StatusType(((Schedule*)DS)->getAllLecturesByCourse(courseID, hours, rooms, numOfLectures));
+}
+
+void Quit(void **DS) {
+    DS = nullptr;
+}
+
 
 
